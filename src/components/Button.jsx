8e874fbccb296ac6be709cdd60ddd962cd7d920e
shustov-dmitry <shustov.dm.s@gmail.com>
2025-09-19 12:22:06 +0300

    refactor: change button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
-export const Button = ({text, onClick, style}) => {
+export const Button = ({ text, onClick, style }) => {
     return (
-        <button className="style"
-                type="button"
-                onClick={onClick}
+        <button
+            className={style}
+            type="button"
+            onClick={onClick}
         >
             {text}
         </button>
     );
-}
+};
