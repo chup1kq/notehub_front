35464da43df12f9ae437a5343c5db34de12d57cb
shustov-dmitry <shustov.dm.s@gmail.com>
2025-09-19 12:19:09 +0300

    feat: add base button

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export const Button = ({text, onClick, style}) => {
+    return (
+        <button className="style"
+                type="button"
+                onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+}
