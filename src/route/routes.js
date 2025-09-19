e5baf1077feb6d2756df581fdf981ad8ff53b343
Zinovev Ivan <izinovev77@gmail.com>
2025-09-19 12:24:33 +0300

    feat: react-router-dom

---
+++ src/route/routes.js
@@ -0,0 +1,11 @@
+import {NotFound} from "../components/404";
+import {useRoutes} from "react-router-dom";
+
+export const AppRoutes = () => {
+    return useRoutes([
+        {
+            path: '*',
+            element: <NotFound/>,
+        }
+    ]);
+}
