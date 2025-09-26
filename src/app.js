137ec267c3cf398ec832e578ea2d2436b80edd10
Zinovev Ivan <izinovev77@gmail.com>
2025-09-26 11:19:02 +0300

    feat: app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router} from "react-router-dom";
+
+import {AppRoutes} from "./route/routes";
+
+export const  App = () => {
+    return (
+        <Router>
+            <AppRoutes />
+        </Router>
+    );
+}
