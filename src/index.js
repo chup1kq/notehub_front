522bd4d57cb185cf845633c3fecd4ab78a664398
Zinovev Ivan <izinovev77@gmail.com>
2025-11-14 16:17:13 +0300

    feat: add AuthContext

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import AuthProvider from "./context/AuthContext";
 import {App} from './app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthProvider>
+            <App/>
+        </AuthProvider>
+    </React.StrictMode>
 );
