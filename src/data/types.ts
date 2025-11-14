b646f61f4f6fda8955c90754298c14dd1a024399
Zinovev Ivan <izinovev77@gmail.com>
2025-11-14 16:14:43 +0300

    feat: add Page

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,3 +1,11 @@
+type Page = {
+    currentPage: number;
+    totalPages: number;
+    contentOnPage: number;
+    totalContent: number;
+    content: Array<Note>;
+}
+
 type Note = {
     id: string;
     createdAt: Date;
