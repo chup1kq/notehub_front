416abb48a5d24e0c9b04562664b4221d417a0b8e
shustov-dmitry <shustov.dm.s@gmail.com>
2025-11-14 21:26:18 +0300

    refactor: page 404

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,13 +1,16 @@
 export const NotFound = () => {
     return (
-        <div className="not-found">
-            <div>
-                <h1>404</h1>
-            </div>
-            <div className="not-found-info">
-                <p>Извините, информация, которую вы ищете, не существует или была удалена.</p>
+        <>
+        <div className={"container"}>
+            <div className="not-found">
+                <div>
+                    <h1>404</h1>
+                </div>
+                <div className="not-found-info">
+                    <p>Извините, информация, которую вы ищете, не существует или была удалена.</p>
+                </div>
             </div>
         </div>
-
+        </>
     );
 }
