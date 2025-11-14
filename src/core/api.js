629aeba78f3101ec58ef70c0e518a53abeb369ac
shustov-dmitry <shustov.dm.s@gmail.com>
2025-11-14 13:54:22 +0300

    feat: add templates for backend api

---
+++ src/core/api.js
@@ -0,0 +1,31 @@
+export async function getNote(url: string){
+
+}
+
+export async function getNotes(login: string, page: number) {
+
+}
+
+export async function deleteNote(url: string) {
+
+}
+
+export async function addNote(note: Note) {
+
+}
+
+export async function updateNote(note: Note) {
+
+}
+
+export async function login() {
+
+}
+
+export async function register() {
+
+}
+
+export function logout() {
+
+}
