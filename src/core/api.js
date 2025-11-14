d734e0fe07c82bcf965d91dd3230f79b41f24204
Zinovev Ivan <izinovev77@gmail.com>
2025-11-15 00:29:43 +0300

    feat: implementation register and authentication

--- src/core/api.js
+++ src/core/api.js
@@ -18,12 +18,41 @@ export async function updateNote(note: Note) {
 
 }
 
-export async function login() {
+export async function authentication(user: string, password: string) {
+    const response = await fetch('http://localhost:8081/api/v1/auth/login', {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({login: user, password: password}),
+    });
 
+    if (response.ok) {
+        return await response.text();
+    } else {
+        throw new Error("Ошибка входа: проверьте логин и пароль.");
+    }
 }
 
-export async function register() {
+export async function register(user: string, password: string) {
+    const response = await fetch('http://localhost:8081/api/v1/user/register', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({login: user, password: password}),
+    });
 
+    if (response.ok) {
+        alert("Регистрация успешна!");
+        return await response.text();
+    } else if (response.status === 409) {
+        alert("Ошибка регистрации: пользователь с таким логином уже есть.")
+        throw new Error("Ошибка регистрации: пользователь с таким логином уже есть.");
+    } else {
+        alert("Ошибка регистрации: попробуйте ещё раз.");
+        throw new Error("Ошибка регистрации: попробуйте ещё раз.");
+    }
 }
 
 export function logout() {
--- src/pages/Authorization.jsx
+++ src/pages/Authorization.jsx
@@ -4,6 +4,7 @@ import {BsMoonFill, BsSunFill} from "react-icons/bs";
 import {useTheme} from "../hooks/useTheme";
 import {useAuth} from "../context/AuthContext";
 import {PasswordInput} from "../components/PasswordInput";
+import {authentication, register} from "../core/api";
 
 const apiUrl = 'http://localhost:8081/api/v1';
 
@@ -35,14 +36,27 @@ export const Authorization = () => {
     }
 
     const handleLogin = async () => {
-        //Заглушка
-        setUser(login);
-        setToken(login);
-        navigate('/');
+        try {
+            const response = await authentication(login, password);
+
+            setUser(login);
+            setToken(response);
+            navigate('/');
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
     const handleRegister = async () => {
+        try {
+            const response = await register(login, password);
 
+            setUser(login);
+            setToken(response);
+            navigate('/');
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
     const handleSubmit = async () => {
