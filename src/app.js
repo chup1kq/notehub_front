94e9d544bf53261dbddf1f41353c116ae68aa02f
shustov-dmitry <shustov.dm.s@gmail.com>
2025-11-14 13:19:53 +0300

    feat: add hiding header

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, useLocation} from "react-router-dom";
 
 import {AppRoutes} from "./route/routes";
+import {Header} from "./components/Header";
 
-export const  App = () => {
+import './static/styles/main.scss'
+
+export const App = () => {
+
     return (
         <Router>
-            <AppRoutes />
+            <AppContent />
         </Router>
+    );
+}
+
+const AppContent = () => {
+    const location = useLocation();
+    const hideHeaderPaths = ['/register', '/login'];
+    const shouldShowHeader = !hideHeaderPaths.includes(location.pathname);
+
+    return (
+        <>
+            {shouldShowHeader && <Header/>}
+            <AppRoutes />
+        </>
     );
 }
